9c5e6a1489942185c9a389772282536503db02cc
engineve <yeindap@gmail.com>
2020-11-20 15:49:58 +0900

    UPDATE simpleWithHtml/app.ts
    type alias and interface

--- simpleWithHtml/app.js
+++ simpleWithHtml/app.js
@@ -1,13 +1,16 @@
-"use strict";
 // tsc --init  --> tsconfig.json 
 var numElement = document.getElementById('num'); // type casting, HTMLInputElement is general type. let typescript know this element has a value
 var buttonElement = document.querySelector('button'); // ! : we know the statement in front is not null // can be null, strictNullChecks by tsconfig.json
-// const buttonElement: HTMLButtonElement
+;
 function convert(num) {
     return +num;
+}
+function display(itemObj, item1Obj) {
+    console.log(itemObj.val, item1Obj.val);
 }
 buttonElement.addEventListener('click', function () {
     var num = numElement.value; // HTMLInputElement.value: string
     var result = convert(num);
     console.log(result);
+    display({ val: 5 }, { val: 10 });
 });
--- simpleWithHtml/app.ts
+++ simpleWithHtml/app.ts
@@ -4,8 +4,16 @@ const numElement = document.getElementById('num') as HTMLInputElement;  // type 
 const buttonElement = document.querySelector('button')!; // ! : we know the statement in front is not null // can be null, strictNullChecks by tsconfig.json
 // const buttonElement: HTMLButtonElement
 
+type numOrstr=number | string
+type Tobj={val:number};
+interface Iobj {val:number};
+
 function convert(num : number | string) {  
   return +num;
+}
+
+function display(itemObj: Tobj, item1Obj:Iobj){
+  console.log(itemObj.val, item1Obj.val);
 }
 
 buttonElement.addEventListener('click', () => {
@@ -13,5 +21,6 @@ buttonElement.addEventListener('click', () => {
 
   const result = convert(num);
   console.log(result);
+  display({val:5},{val:10}); // 5 10
 });
 
