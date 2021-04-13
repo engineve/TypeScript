e9c94b7e684a611659dc395eae3ae45d74a4b30e
enginepdf <yeindap@gmail.com>
2021-04-13 21:00:20 +0900

    Create object.ts
    object in typescript

---
+++ object.ts
@@ -0,0 +1,15 @@
+// information:object = { }    
+// information:{}= 
+const information: {  
+    name: string;
+    price: number;
+    functions: string[];
+    production: [number, string]; // any[]
+    weight: number;
+} = {
+    name: 'Macbook',
+    price: 2000, // dollar
+    functions: ['Helping your works', 'Playing music'],
+    production: [2016, 'China'],
+    weight: 1.79  // kg
+}
